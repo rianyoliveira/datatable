c9b34d5fe53f64167005ccd29f3dabcb7315d664
Eduardo Pereira de Sousa <edupsousa@gmail.com>
2015-09-28 11:38:45 -0300

    TDD: Add Collumn

--- src/com/edupsousa/datatable/DataTable.java
+++ src/com/edupsousa/datatable/DataTable.java
@@ -1,13 +1,26 @@
 package com.edupsousa.datatable;
 
+import java.util.HashMap;
+
 public class DataTable {
 
+	public static final int TYPE_INT = 0;
+	private HashMap<String, Integer> columnsTypes = new HashMap<String, Integer>(); 
+	
 	public int columnsCount() {
-		return 0;
+		return columnsTypes.size();
 	}
 
 	public int rowsCount() {
 		return 0;
+	}
+
+	public void addCollumn(String name, int type) {
+		columnsTypes.put(name, type);
+	}
+
+	public boolean hasCollumn(String name) {
+		return columnsTypes.containsKey(name);
 	}
 
 }
--- test/com/edupsousa/datatable/test/DataTableTest.java
+++ test/com/edupsousa/datatable/test/DataTableTest.java
@@ -2,17 +2,31 @@ package com.edupsousa.datatable.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.edupsousa.datatable.DataTable;
 
 public class DataTableTest {
 
+	DataTable dt;
+	
+	@Before
+	public void instanceDataTable() {
+		dt = new DataTable();
+	}
+	
 	@Test
 	public void emptyDataTable() {
-		DataTable dt = new DataTable();
 		assertEquals(0, dt.columnsCount());
 		assertEquals(0, dt.rowsCount());
+	}
+	
+	@Test
+	public void addCollumn() {
+		dt.addCollumn("id", DataTable.TYPE_INT);
+		assertEquals(1, dt.columnsCount());
+		assertTrue(dt.hasCollumn("id"));
 	}
 	
 }
